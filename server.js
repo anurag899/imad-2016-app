a61bd5a0732e0f3f2a0aeb0c30bc6dcac61d1da1
Anurag Mishra <anurag.mishra899@gmail.com>
2016-10-05 18:48:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile("I am happy to do this");
+    res.sendFile('I am happy to do this');
 });
 
 app.get('/ui/style.css', function (req, res) {
