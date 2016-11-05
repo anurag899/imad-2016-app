fd18c5f6581ddb1b57dc758b3d105f3b1381a122
Anurag <anurag.mishra899@gmail.com>
2016-11-05 21:55:13 +0530

    practice

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pg = require('pg');
+
+var config={
+	user: 'postgres',
+	database: 'postgres',
+	host: 'localhost',
+	password: 123
+};
+
+if (typeof Promise == 'undefined') {
+  global.Promise = require('promise-polyfill')
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -73,7 +85,7 @@ var articles={
 	}	
 };
 function createTemp(data){
-	var title=data.title;
+	var title=data.title;	
 	var heading=data.heading;
 	var date=data.date;
 	var content=data.content;
@@ -90,7 +102,7 @@ function createTemp(data){
 				        	"<div><a href='/'>Home</a></div>"+
 				        	"<hr/>"+
 				        	"<h3>"+ heading +"</h3>"+
-				        	"<div>"+ date +"</div>"+
+				        	"<div>"+ date.toDateString() +"</div>"+
 				        	"<div>"+ content +"</div>"+
 				        "</div>"+
 				    "</body>"+
@@ -117,9 +129,34 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName',function(req,res){
-	var articleName=req.params.articleName;
-    res.send(createTemp(articles[articleName]));
+var pool= new pg.Pool(config);
+
+app.get('/test-db',function(req,res){
+	pool.query("SELECT * FROM article",function(err,result){
+		if(err){
+			res.status(500).send(err.toString());
+		}
+		else{
+			res.send(JSON.stringify(result.rows[0].title));
+		}
+	});
+});
+app.get('/articles/:articleName',function(req,res){
+	pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName+"'",
+		function(err,result){
+			if(err){
+				res.status(500).send(err.toString());
+			}
+			else{
+				if(result.rows.length === 0){
+					res.status(404).send("Article not found");
+				}
+				else{
+					var articleData = result.rows[0];
+					res.send(createTemp(articleData));	
+				}
+			}
+		});
 });
 
 app.get('/ui/style.css', function (req, res) {
