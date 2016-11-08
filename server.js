253796c104a641719137fbdfe58c4529216522cd
Anurag <anurag.mishra899@gmail.com>
2016-11-09 03:45:51 +0530

    blog website

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var pg = require('pg');
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config={
 	user: 'postgres',
@@ -16,74 +19,43 @@ if (typeof Promise == 'undefined') {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json()); 
+app.use(session({
+	secret: 'randomValue',
+	cookie: {maxAge: 1000*60*60*24*30}
+}));
 
-var articles={	
-'article-one' :{
-	title:'Article-one | Anurag',
-	heading:'Article-one',
-	date:'Sep 5,2016',
-	content: 
-	"<p>A strong memory depends on the health and vitality of your brain."+ 
-	"Whether you are a "+
-	"student studying for final exams, a working professional interested"+ 
-	"in doing all you can to stay mentally sharp, or a senior looking to preserve and enhance"+ 
-	"your grey matter as you age, there are lots of things "+
-	"you can do to improve your memory and mental performance.</p>"+
-	"<p>A strong memory depends on the health and vitality of your brain. Whether "+
-	"you are a student studying for final exams, a working professional interested "+
-	"in doing all you can to stay mentally sharp, or a senior looking to preserve and"+ 
-	"enhance your grey matter as you age, there are lots of things "+
-	"you can do to improve your memory and mental performance.</p>"+
-	"<p>A strong memory depends on the health and vitality of your brain. Whether"+ 
-	"you are a student studying for final exams, a working professional interested "+
-	"in doing all you can to stay mentally sharp, or a senior looking to preserve and"+ 
-	"enhance your grey matter as you age, there are lots of things "+
-	"you can do to improve your memory and mental performance.</p>" },
-'article-two' :{
-	title:'Article-two | Anurag',
-	heading:'Article-two',
-	date:'Sep 10,2016',
-	content: 
-	"<p>A strong memory depends on the health and vitality of your brain."+ 
-	"Whether you are a "+
-	"student studying for final exams, a working professional interested"+ 
-	"in doing all you can to stay mentally sharp, or a senior looking to preserve and enhance"+ 
-	"your grey matter as you age, there are lots of things "+
-	"you can do to improve your memory and mental performance.</p>"+
-	"<p>A strong memory depends on the health and vitality of your brain. Whether "+
-	"you are a student studying for final exams, a working professional interested "+
-	"in doing all you can to stay mentally sharp, or a senior looking to preserve and"+ 
-	"enhance your grey matter as you age, there are lots of things "+
-	"you can do to improve your memory and mental performance.</p>"+
-	"<p>A strong memory depends on the health and vitality of your brain. Whether"+ 
-	"you are a student studying for final exams, a working professional interested "+
-	"in doing all you can to stay mentally sharp, or a senior looking to preserve and"+ 
-	"enhance your grey matter as you age, there are lots of things "+
-	"you can do to improve your memory and mental performance.</p>" 
-	},
-'article-three' :{
-	title:'Article-Three | Anurag',
-	heading:'Article-Three',
-	date:'Sep 20,2016',
-	content: 
-	"<p>A strong memory depends on the health and vitality of your brain."+ 
-	"Whether you are a "+
-	"student studying for final exams, a working professional interested"+ 
-	"in doing all you can to stay mentally sharp, or a senior looking to preserve and enhance"+ 
-	"your grey matter as you age, there are lots of things "+
-	"you can do to improve your memory and mental performance.</p>"+
-	"<p>A strong memory depends on the health and vitality of your brain. Whether "+
-	"you are a student studying for final exams, a working professional interested "+
-	"in doing all you can to stay mentally sharp, or a senior looking to preserve and"+ 
-	"enhance your grey matter as you age, there are lots of things "+
-	"you can do to improve your memory and mental performance.</p>"+
-	"<p>A strong memory depends on the health and vitality of your brain. Whether"+ 
-	"you are a student studying for final exams, a working professional interested "+
-	"in doing all you can to stay mentally sharp, or a senior looking to preserve and"+ 
-	"enhance your grey matter as you age, there are lots of things "+
-	"you can do to improve your memory and mental performance.</p>" 
-	}	
-};
+function sendLogout(info){
+	var content="<html>"+
+				"<head>"+
+				"<style>"+
+				".msg{"+
+					"background-color:white;}"+
+				"#link{"+
+					"width:100px;"+
+					"background-color: #008CBA;"+
+					"height: 40px;"+
+					"color: white;"+
+					"box-shadow: 10px 5px 5px  #888888;"+
+					"border-radius: 5px;"+
+				"}"+
+				"</style>"+
+				"</head>"+
+				"<body>"+
+				"<script type='text/javascript'>"+
+					"function home(){"+
+						"location.href='/';"+
+					"}"+
+				"</script>"+
+				"<div class='msg'>"+
+				"<p style='padding:20px 0 20px 60px;font-size:15px;'>"+info+	
+				"<input type='button' id='link' style='float:right;width:80px;'onclick='home()' value='close'>"+"</p>"+	
+				"</div>"+
+				"</body>"+
+				"</html>";
+	return content;
+}
+
 function createTemp(data){
 	var title=data.title;	
 	var heading=data.heading;
@@ -93,22 +65,163 @@ function createTemp(data){
 			"<!doctype html>"+
 				"<html>"+
 				    "<head>"+
-				    	"<title> "+ title +"</title>"+
-				    	"<meta name='viewport' content='width-device-width, initial-scale=1'/>"+
-				        "<link href='/ui/style.css' rel='stylesheet' />"+
+				    	"<link href='/ui/style.css' rel='stylesheet' />"+
+					        "<title>SocialPedia</title>"+
+					          "<meta charset='utf-8'>"+
+					          "<meta name='viewport' content='width=device-width, initial-scale=1'>"+
+					          "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>"+
+					          "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>"+
+					          "<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>"+
 				    "</head>"+
-				    "<body>"+
+				    "<body onload='checkLogin()'>"+
+				    "<nav class='navbar navbar-default' style='margin-bottom:0;'>"+
 				        "<div class='container'>"+
-				        	"<div><a href='/'>Home</a></div>"+
-				        	"<hr/>"+
-				        	"<h3>"+ heading +"</h3>"+
-				        	"<div>"+ date.toDateString() +"</div>"+
-				        	"<div>"+ content +"</div>"+
+				          "<div class='navbar-header'>"+
+				          "<a class='navbar-brand' href='/'>SocialPedia</a>"+
+				      "</div>"+
+				      "<div class='collapse navbar-collapse' id='myNavbar'>"+
+				       "<ul class='nav navbar-nav navbar-right'>"+
+				       "<li><a href='/'>Home</a></li>"+
+				         "<li><a href='' data-toggle='modal' data-target='#myModal' id='login_area'><span class='glyphicon glyphicon-log-in'></span> </a></li>"+
+				            "</ul>"+
+				          "</div>"+
 				        "</div>"+
+				      "</nav>"+
+				        "<div class='container' style='height:100%;margin-top:50px;'>"+		
+				        	"<span class=' text-center' style='padding:0 30px 0 30px;'><h1>"+ heading +"</h1>"+
+				        	"<div>"+ date.toDateString() +"</div>"+
+				        	"<h5 ><span class='label label-primary'>"+title+"</span></h5><br></span>"+
+				        	"<div style='font-size:22px;font-family:verdana;text-align:justify;margin-top:60px;padding:0 90px 300px 90px; text-justify: inter-word;'>"	+
+				        	 content +"</div>"+
+				        "</div>"+
+				        "<footer class='container-fluid text-center' style='margin-right:0;margin-left:0;background-color:#e7e7e7;color:black;'>"+
+					          "<p>Copyright@ anur@g</p>"+
+					     "</footer>"+
+					     
+					     "<script type='text/javascript' src='/ui/main.js'>"+
+        				"</script>"+
+        				"<div class='modal fade' id='myModal' role='dialog'>"+
+    						"<div class='modal-dialog'>"+
+      						"<!-- Modal content-->"+
+      							"<div class='modal-content'>"+
+        							"<div class='modal-header'>"+
+          							"<button type='button' class='close' data-dismiss='modal'>&times;</button>"+
+          							"<h4 class='modal-title'>Sign in/Sign out</h4>"+
+       								 "</div>"+
+							        "<div class='modal-body'>"+
+							          "<h2>Login</h2>"+
+							          "<form>"+
+							            "Username:<br>"+
+							            "<input type='text' id='username' placeholder='username'>"+
+							            "<br>"+
+							            "Password:<br>"+
+							            "<input type='password' id='password' placeholder='password'>"+
+							            "<br>"+
+							            "<input type='submit' id='login_btn' Value='Login'>"+
+							            "<input type='submit' id='register_btn' Value='register'>"
+							            "<br>"+
+							          "</form>"+
+							        "</div>"+
+					              "</div>"+
+					             "</div>"+
 				    "</body>"+
 				"</html>";
 	return htmlTemp;
 }
+
+function getArticle(title){
+	var htmlTemp=
+			"<!doctype html>"+
+				"<html>"+
+				    "<head>"+
+				    	"<link href='/ui/style.css' rel='stylesheet' />"+
+					        "<title>SocialPedia</title>"+
+					          "<meta charset='utf-8'>"+
+					          "<meta name='viewport' content='width=device-width, initial-scale=1'>"+
+					          "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>"+
+					          "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>"+
+					          "<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>"+
+				    "</head>"+
+				    "<body onload='checkLogin()'>"+
+				    "<script type='text/javascript' src='/ui/main.js'>"+
+        			"</script>"+
+				    "<nav class='navbar navbar-default' style='margin-bottom:0;'>"+
+				        "<div class='container'>"+
+				          "<div class='navbar-header'>"+
+				          "<a class='navbar-brand' href='/'>SocialPedia</a>"+
+				      "</div>"+
+				      "<div class='collapse navbar-collapse' id='myNavbar'>"+
+				       "<ul class='nav navbar-nav navbar-right'>"+
+				       "<li><a href='/'>Home</a></li>"+
+				         "<li><a href='' data-toggle='modal' data-target='#myModal' id='login_area'><span class='glyphicon glyphicon-log-in'></span> </a></li>"+
+				            "</ul>"+
+				          "</div>"+
+				        "</div>"+
+				      "</nav>"+
+				        "<div class='container' style='height:100%;margin-top:50px;'>"+		
+				    		"<h4>Author:   "+title+"</h4>"+
+				        	"<div class='form-group'>"+
+							  "<label for='usr'>Title:</label>"+
+  								"<input type='text' class='form-control' id='usr' placeholder='Write your title'>"+
+							"</div>"+
+				        	"<div class='form-group'>"+
+							  "<label for='comment'>Write Here:</label>"+
+  								"<textarea class='form-control' rows='10' placeholder='Tell your story'></textarea>"+
+							"</div>"+
+							"<input type='submit' value='submit'>"
+				        "<footer class='container-fluid text-center' style='margin-right:0;margin-left:0;background-color:#e7e7e7;color:black;'>"+
+					          "<p>Copyright@ anur@g</p>"+
+					     "</footer>"+
+					    "<div class='modal fade' id='myModal' role='dialog'>"+
+    						"<div class='modal-dialog'>"+
+      						"<!-- Modal content-->"+
+      							"<div class='modal-content'>"+
+        							"<div class='modal-header'>"+
+          							"<button type='button' class='close' data-dismiss='modal'>&times;</button>"+
+          							"<h4 class='modal-title'>Sign in/Sign out</h4>"+
+       								 "</div>"+
+							        "<div class='modal-body'>"+
+							          "<h2>Login</h2>"+
+							          "<form>"+
+							            "Username:<br>"+
+							            "<input type='text' id='username' placeholder='username'>"+
+							            "<br>"+
+							            "Password:<br>"+
+							            "<input type='password' id='password' placeholder='password'>"+
+							            "<br>"+
+							            "<input type='submit' id='login_btn' Value='Login'>"+
+							            "<input type='submit' id='register_btn' Value='register'>"
+							            "<br>"+
+							          "</form>"+
+							        "</div>"+
+					              "</div>"+
+					             "</div>"+
+				    "</body>"+
+				"</html>";
+
+	return htmlTemp;
+}
+
+app.get('/writeStory', function (req, res) {
+   // Check if the user is logged in
+    if (req.session && req.session.auth && req.session.auth.userId) {
+        // First check if the article exists and get the article-id
+        pool.query('SELECT username from "user" where id = $1', [req.session.auth.userId], function (err, result) {
+            if (err) {
+                res.status(500).send(err.toString());
+            } else {
+                if (result.rows.length === 0) {
+                    res.status(400).send('User not found');
+                } else {
+                    var user = result.rows[0].username;
+                    res.send(getArticle(user));
+                }
+            }
+       });     
+    } else {
+        res.status(403).send('Only logged in users can write');
+    }
+});
 
 var names=[];
 app.get('/submit',function(req,res){
@@ -117,6 +230,116 @@ app.get('/submit',function(req,res){
 	res.send(JSON.stringify(names));
 });
 
+function hash(input , salt){
+	var hashed = crypto.pbkdf2Sync(input,salt, 10000, 512, 'sha512');
+	return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
+}
+
+app.get('/hash/:input',function(req,res){
+	var hashed = hash(req.params.input,'this-is-ramdom-string');
+	res.send(hashed);
+});
+
+app.post('/create-user',function(req,res){
+	var username=req.body.username;
+	var password=req.body.password;
+
+	var salt=crypto.randomBytes(128).toString('hex');
+	var dbString = hash(password,salt);
+
+	pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',
+		[username,dbString],function(err,result){
+			if(err){
+				res.status(500).send(err.toString());
+			}
+			else{
+				res.status(200).send("user is succefull logged in");
+			}
+ 	});
+});
+
+app.post('/submit-comment/:articleName', function (req, res) {
+   // Check if the user is logged in
+    if (req.session && req.session.auth && req.session.auth.userId) {
+        // First check if the article exists and get the article-id
+        pool.query('SELECT * from article where title = $1', [req.params.articleName], function (err, result) {
+            if (err) {
+                res.status(500).send(err.toString());
+            } else {
+                if (result.rows.length === 0) {
+                    res.status(400).send('Article not found');
+                } else {
+                    var articleId = result.rows[0].id;
+                    // Now insert the right comment for this article
+                    pool.query(
+                        "INSERT INTO comment (comment, article_id, user_id) VALUES ($1, $2, $3)",
+                        [req.body.comment, articleId, req.session.auth.userId],
+                        function (err, result) {
+                            if (err) {
+                                res.status(500).send(err.toString());
+                            } else {
+                                res.status(200).send('Comment inserted!')
+                            }
+                        });
+                }
+            }
+       });     
+    } else {
+        res.status(403).send('Only logged in users can comment');
+    }
+});
+
+
+app.post('/login',function(req,res){
+	var username=req.body.username;
+	var password=req.body.password;
+
+	pool.query('SELECT * FROM "user" WHERE username= $1',[username]
+		,function(err,result){
+			if(err){
+				res.status(500).send(err.toString());
+			}
+			else{
+				if(result.rows.length === 0){
+					res.send(403).send("username/password is invalid");
+				}
+				else{
+					
+					var dbString=result.rows[0].password;
+					var salt=dbString.split('$')[2];
+					var hashed = hash(password,salt);
+					if(hashed === dbString){
+						req.session.auth={userId:result.rows[0].id};
+						res.send("user is logged in");
+					}
+					else{
+						res.send(403).send("username/password is invalid");
+					}
+				}
+			}
+
+		});
+});
+
+app.get('/check-login',function(req,res){
+	if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
+});
+
+app.get('/logout',function(req,res){
+	delete req.session.auth;
+	res.send(sendLogout('You are now logged out.Hope you enjoyed our service.'));
+});
 
 var counter=0;
 app.get('/counter', function (req, res) {
@@ -159,6 +382,42 @@ app.get('/articles/:articleName',function(req,res){
 		});
 });
 
+app.get('/getArticle',function(req,res){
+	pool.query("SELECT * FROM article ORDER BY id DESC limit 3",
+		function(err,result){
+			if(err){
+				res.status(500).send(err.toString());
+			}
+			else{
+				if(result.rows.length === 0){
+					res.status(404).send("Article not found");
+				}
+				else{
+					var articleData = result.rows;
+					res.send(JSON.stringify(articleData));	
+				}
+			}
+		});
+});
+
+app.get('/getList',function(req,res){
+	pool.query("SELECT heading,title FROM article ORDER BY id DESC limit 20",
+		function(err,result){
+			if(err){
+				res.status(500).send(err.toString());
+			}
+			else{
+				if(result.rows.length === 0){
+					res.status(404).send("Article not found");
+				}
+				else{
+					var articleData = result.rows;
+					res.send(JSON.stringify(articleData));	
+				}
+			}
+		});
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -167,8 +426,8 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/Upload.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui','Upload.jpg'));
+app.get('/ui/pic.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui','pic.jpg'));
 });
 
 
--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,104 @@
-var button=document.getElementById('counter');
-var counter=0;
-button.onclick=function(){
-	 //create request object
-	 var request=new XMLHttpRequest();
-	 //capture the reponse and store it in variable
-	 request.onreadystatechange =	function(){
-	 	if(request.readyState === XMLHttpRequest.DONE){
-	 		//take some action
-	 		if(request.status === 200){
-	 			var counter=request.responseText;
-	 			var span=document.getElementById('count');
-	 			span.innerHTML=counter.toString();
-	 		}
-	 	}
-	 };
-	 request.open('GET','http://localhost:5000/counter',true);
-	 request.send(null);
-};
+function loadLoginForm () {
+    // Submit username/password to login
+    var loginarea=document.getElementById('login_area');
+    loginarea.innerHTML="Login/Logout";
+    var submit = document.getElementById('login_btn');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+              checkLogin();
+          }  
+          // Not done yet
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', 'http://localhost:5000/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+    };
+    
+    var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else if(request.status===403) {
+                  alert('us');
+                  register.value = 'Register';
+              }
+              else{
+                //alert('use');
+              }
+          }
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', 'http://localhost:5000/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
+    };
+}
 
-var bt=document.getElementById('sub');
-bt.onclick=function(){
+function loadLoggedInUser (username) {
+    var loginArea = document.getElementById('login_area');
+    loginArea.innerHTML ="Hii "+username+"(Logout)";
+    loginArea.href="/logout";
+    var loginarea=document.getElementById('story');
+    loginarea.href='/writeStory';
+    
+}
+
+function checkLogin(){
+	var request = new XMLHttpRequest();
+	request.onreadystatechange=function(){
+		    if (request.readyState === XMLHttpRequest.DONE) { 
+		        if (request.status === 200) {
+                	loadLoggedInUser(this.responseText);
+	            } else {
+	                loadLoginForm();
+	            }
+	        }
+        };
+    request.open('GET', 'http://localhost:5000/check-login', true);
+    request.send(null);
+}
+
+function fetchArticle(){
 	 //create request object
 	 var request=new XMLHttpRequest();
 	 //capture the reponse and store it in variable
@@ -27,20 +106,64 @@ bt.onclick=function(){
 	 	if(request.readyState === XMLHttpRequest.DONE){
 	 		//take some action
 	 		if(request.status === 200){
-	 			var names=request.responseText;
-	 			names=JSON.parse(names);
-	 			var list='';
-	 			for(var i=0;i<names.length;i++){
-	 				list+='<li>'+names[i]+'</li>';
-	 			}
-	 			var ui=document.getElementById('namelist');
-	 			ui.innerHTML=list;
+	 		  var names =request.responseText;
+	 		  names=JSON.parse(names);
+	 		  var blogs='',i;
+	 		  var list='';
+	 		  for(i=0;i<names.length;i++){
+				var content="<h4><small>RECENT POSTS</small></h4>"+
+			    "<hr>"+
+			    "<h2 id ='title-one'>"+names[i].heading+"</h2>"+
+			    "<h5 ><span class='glyphicon glyphicon-time'></span><span id='date-one'>"+" "+names[i].date+"</span> </h5>"+
+			    "<h5 ><span id='auth-one' class='label label-primary'>"+names[i].title+"</span></h5><br>"+
+			    "<p id ='content-one'>"+names[i].content.substring(0,300)+"..."+
+			    "<a href='/articles/"+names[i].title+"'>"+
+			    "Read more</a>"+
+			    "</p>"+
+			    "<br><br>";
+	 		  	blogs+=content+"<hr>";
+	 		  	list+="<li><a href=''>"+names[i].heading+"</a></li>";
+	 		  }
+	 		 	var blog=document.getElementById('blog');
+	 		  	blog.innerHTML=blogs;
+	 		  	var blist=document.getElementById('list');
+	 		  	blist.innerHTML=list;
 	 		}
 	 	}
 	 
 	 };
-	var text=document.getElementById('name');
-	var name=text.value ;
-	request.open('GET','http://localhost:5000/submit?name='+name,true);
+	request.open('GET','http://localhost:5000/getArticle',true);
 	request.send(null);
-	};
+}
+
+function fetchList(){
+	 //create request object
+	 var request=new XMLHttpRequest();
+	 //capture the reponse and store it in variable
+	 request.onreadystatechange =	function(){
+	 	if(request.readyState === XMLHttpRequest.DONE){
+	 		//take some action
+	 		if(request.status === 200){
+	 		  var names =request.responseText;
+	 		  names=JSON.parse(names);
+	 		  var list='';
+	 		  for(i=0;i<names.length;i++){
+				list+="<li><a href='/articles/"+names[i].title+"'>"+names[i].heading+"</a></li>";
+	 		  }
+	 		 	var blist=document.getElementById('list');
+	 		  	blist.innerHTML=list;
+	 		}
+	 	}
+	 
+	 };
+	request.open('GET','http://localhost:5000/getList',true);
+	request.send(null);
+}
+
+//for login check
+checkLogin();
+
+//for articles
+fetchArticle();
+//for list
+fetchList();
