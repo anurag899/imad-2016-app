f118485f850baa59d9a3fe452bafb2cba1207e41
Anurag Mishra <anurag.mishra899@gmail.com>
2016-11-12 11:11:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ function getArticle(title){
 					          "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>"+
 					          "<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>"+
 				    "</head>"+
-				    "<body>"+
+				    "<body onload='writeStory()'>"+
 				    "<script type='text/javascript' src='/ui/main.js'>"+
         			"</script>"+
 				    "<nav class='navbar navbar-default' style='margin-bottom:0;'>"+
